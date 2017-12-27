daced2891fb82b228cd511450577075c93dace58
KseniaStarPIbd21 <anorecsichka@yandex.ru>
2017-12-27 23:31:53 +0400

    save word

---
+++ EnglishTeacher/Scripts/login.js
@@ -0,0 +1,36 @@
+﻿var ex = new extenApi();
+var tokenKey = "tokenInfo";
+var email = getCookie("username");
+var password = getCookie("password");
+if (email || password) {
+    let user = getCookie("username");
+    let pass = getCookie("password");
+    ex.login(user, pass, function (data) {
+        sessionStorage.setItem(tokenKey, data.access_token);
+        document.location.href = "main.html";
+    }, function (jqXHR, textStatus, errorThrown) {
+        alert(jqXHR.responseText || textStatus);
+    });
+
+} else {
+    $(document).ready(function () {
+
+        $('#but-Sign-In').click(function (e) {
+            e.preventDefault();
+            let email = $('#emailLogin').val();
+            let password = $('#passwordLogin').val();
+            $('#preloader').css('display', 'block');
+            ex.login(email, password, function (data) {
+                sessionStorage.setItem(tokenKey, data.access_token);
+                setCookie("username", email);
+                setCookie("password", password);
+                $('#preloader').css('display', 'none');
+                document.location.href = "main.html";
+            }, function (jqXHR, textStatus, errorThrown) {
+                $('#preloader').fadeOut('1000');;
+                $('#infoBlock').fadeIn('1000');
+                $('#message').text(jqXHR.responseJSON["error_description"]);
+            });
+        });
+    });
+}
--- EnglishTeacher/Scripts/popup.js
+++ EnglishTeacher/Scripts/popup.js
@@ -1,144 +1,128 @@
-﻿//document.addEventListener('DOMContentLoaded', function () {
-//    var checkPageButton = document.getElementById('checkPage');
-//    checkPageButton.addEventListener('click', function () {
-//        chrome.tabs.getSelected(null, function (tab) {
-//            d = document;
-
-//            var f = d.createElement('form');
-//            f.action = 'http://gtmetrix.com/analyze.html?bm';
-//            f.method = 'post';
-//            var i = d.createElement('input');
-//            i.type = 'hidden';
-//            i.name = 'url';
-//            i.value = tab.url;
-//            f.appendChild(i);
-//            d.body.appendChild(f);
-//            f.submit();
-//        });
-//    }, false);
-//}, false);
-
-//var user = getCookie("username");
-//var password = getCookie("password");
-//if (user || password) {
-//    var email = getCookie("username");
-//    var password = getCookie("password");
-//    login(email, password, preloader, function (data) {
-//        sessionStorage.setItem(tokenKey, data.access_token);
-//        document.location.href = "main.html";
-//    });
-
-//} else {
-$(document).ready(function () {
+﻿$(document).ready(function () {
     var ex = new extenApi();
     var tokenKey = "tokenInfo";
-    $('#but-Sign-In').click(function (e) {
-        e.preventDefault();
-        var email = $('#emailLogin').val();
-        var password = $('#passwordLogin').val();
-        $('#wrapped').css('display', 'block');
-        ex.login(email, password, function (data) {
-            sessionStorage.setItem(tokenKey, data.access_token);
-            setCookie("username", email);
-            setCookie("password", password);
-            $('#wrapped').css('display', 'none');
-            document.location.href = "main.html";
-        }, function (jqXHR, textStatus, errorThrown) {
+<<<<<<< HEAD
+<<<<<<< HEAD
+    //$('#but-Sign-In').click(function (e) {
+    //    e.preventDefault();
+    //    let email = $('#emailLogin').val();
+    //    let password = $('#passwordLogin').val();
+    //    $('#wrapped').css('display', 'block');
+    //    ex.login(email, password, function (data) {
+    //        sessionStorage.setItem(tokenKey, data.access_token);
+    //        setCookie("username", email);
+    //        setCookie("password", password);
+    //        $('#wrapped').css('display', 'none');
+    //        document.location.href = "main.html";
+    //    }, function (jqXHR, textStatus, errorThrown) {
+    //        $('#wrapped').css('display', 'none');
+    //        alert(jqXHR.responseText || textStatus);
+    //    });
+    //});
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:54049/api/Models/GetWords',
+        //     datatype: 'jsonp',
+        beforeSend: function (xhr) {
+            //     preloader.css('display', 'block');
+        },
+        success: function (data) {
+            //   console.log(data[Math.round(0 - 0.5 + Math.random() * (data.length))]);
+
+            let r = Math.round(0 - 0.5 + Math.random() * (data.length));
+            document.getElementById("newWord").innerHTML = data[r].word1;
+            document.getElementById("transcription").innerHTML = data[r].transcription;
+            document.getElementById("translate").innerHTML = data[r].translate;
+
+            //    $('#lbl').text = (data[Math.round(0 - 0.5 + Math.random() * (data.length))].word1);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
             alert(jqXHR.responseText || textStatus);
-        });
-        
+        }
     });
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:54049/api/Models/GetWords',
-            //     datatype: 'jsonp',
-            beforeSend: function (xhr) {
-                //     preloader.css('display', 'block');
-            },
-            success: function (data) {
-                //   console.log(data[Math.round(0 - 0.5 + Math.random() * (data.length))]);
+=======
+=======
+>>>>>>> e9879562c1a8ec453f44adaef41cad8dd2928b96
+    
+    //$.ajax({
+    //    type: 'GET',
+    //    url: 'http://localhost:54049/api/Models/GetWords',
+    //    //     datatype: 'jsonp',
+    //    beforeSend: function (xhr) {
+    //        //     preloader.css('display', 'block');
+    //    },
+    //    success: function (data) {
+    //        //   console.log(data[Math.round(0 - 0.5 + Math.random() * (data.length))]);
 
-                var r = Math.round(0 - 0.5 + Math.random() * (data.length));
-                document.getElementById("newWord").innerHTML = data[r].word1;
-                document.getElementById("transcription").innerHTML = data[r].transcription;
-                document.getElementById("translate").innerHTML = data[r].translate;
+    //        let r = Math.round(0 - 0.5 + Math.random() * (data.length));
+    //        document.getElementById("newWord").innerHTML = data[r].word1;
+    //        document.getElementById("transcription").innerHTML = data[r].transcription;
+    //        document.getElementById("translate").innerHTML = data[r].translate;
 
-                //    $('#lbl').text = (data[Math.round(0 - 0.5 + Math.random() * (data.length))].word1);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert(jqXHR.responseText || textStatus);
+    //        //    $('#lbl').text = (data[Math.round(0 - 0.5 + Math.random() * (data.length))].word1);
+    //    },
+    //    error: function (jqXHR, textStatus, errorThrown) {
+
+    //        alert(jqXHR.responseText || textStatus);
+    //    }
+    //});
+   
+<<<<<<< HEAD
+>>>>>>> ba3c1fe80285efaed5c89ec76a0f8852aa5abe54
+=======
+>>>>>>> e9879562c1a8ec453f44adaef41cad8dd2928b96
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:54049/api/Models/GetThemes',
+        //     datatype: 'jsonp',
+        beforeSend: function (xhr) {
+            // preloader.css('display', 'block');
+        },
+        success: function (data) {
+            // console.log(data);
+            //  var cityData = result.Data;
+            let defaultV = new Option("--Select--", 0, true);
+            $('#selectTheme').empty();
+            $('#selectTheme').append(defaultV);
+            for (let i = 0; i < data.length; i++) {
+                let opt = new Option(data[i].name, data[i].id_theme, false);
+                $('#selectTheme').append(opt);
             }
-        });
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:54049/api/Models/GetThemes',
-            //     datatype: 'jsonp',
-            beforeSend: function (xhr) {
-                // preloader.css('display', 'block');
-            },
-            success: function (data) {
-                // console.log(data);
-                //  var cityData = result.Data;
-                var defaultV = new Option("--Select--", 0, true);
-                $('#selectTheme').empty();
-                $('#selectTheme').append(defaultV);
-                for (var i = 0; i < data.length; i++) {
-                    var opt = new Option(data[i].name, data[i].id_theme, false);
-                    $('#selectTheme').append(opt);
-                }
-                sessionStorage.setItem(tokenKey, data.access_token);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert(jqXHR.responseText || textStatus);
-            }
-        });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText || textStatus);
+        }
+    });
 
-//}
     $('#but-Sign-Up').click(function (e) {
         e.preventDefault();
-        $('#wrapped').css('display', 'block');
+        $('#preloader').css('display', 'block');
         ex.registration($('#inputEmail').val(), $('#inputPassword').val(),
             $('#inputPasswordConfirmation').val(),
             function (data) {
                 alert("Всё норм");
-                $('#wrapped').css('display', 'none'); 
-            },
-            function (data) {
-                alert(data.responseText);
-            }
-        );
-          
+                $('#preloader').css('display', 'none');
+            }, function (jqXHR, textStatus, errorThrown) {
+                $('#preloader').css('display', 'none');
+                alert(jqXHR.responseText || textStatus);
+            });
+
     });
 
-    //var loginData = {
-    //    grant_type: 'password',
-    //    username: $('#emailLogin').val(),
-    //    password: $('#passwordLogin').val()
-    //};
-    //var ajaxData = {
-    //    type: 'POST',
-    //    url: 'http://localhost:54049/Token',
-    //    data: loginData,
-    //    beforeSend: function () {
-    //        preloader.css('display', 'block');
-    //    },
-    //    complete: function () {
-    //        preloader.css('display', 'none');
-    //    },
-    //    success: function (data) {
-    //        sessionStorage.setItem(tokenKey, data.access_token);
-    //        document.location.href = "main.html";
-    //    },
-    //    fail: function (data) {
-    //        alert(data.responseText);
-    //    },
-    //    error: function (jqXHR, textStatus, errorThrown) {
-    //        alert(jqXHR.responseText || textStatus);
-    //    }
-    //};
-
-    //$.ajax(ajaxData);
-
+    $('#send-new-pass').click(function (e) {
+        e.preventDefault();
+        $('#preloader').css('display', 'block');
+        ex.forrgotPassword($('#emailToSend').val(),
+            function (data) {
+                alert("Вам отправлено письмо с новым паролем");
+                $('#preloader').css('display', 'none');
+            }, function (jqXHR, textStatus, errorThrown) {
+                $('#preloader').css('display', 'none');
+                alert(jqXHR.responseText || textStatus);
+            });
+    });
 
     $('#forgot-pass').click(function () {
         $("#main-section").fadeOut('1000');
@@ -146,75 +130,101 @@ $(document).ready(function () {
             $('#add-section').fadeIn('1000');
         }, 500);
     });
+
     $('#cancle').click(function () {
         $("#add-section").fadeOut('1000');
         setTimeout(function () {
             $('#main-section').fadeIn('1000');
         }, 500);
     });
-    $('#send-new-pass').click(function (e) {
-        e.preventDefault();
-        var data = {
-            Email: $('#emailToSend').val()
-        };
 
+
+    //$('#getItemsButton').click(function (e) {
+    //    e.preventDefault();
+    //    $.ajax({
+    //        type: 'GET',
+    //        url: 'http://localhost:54049/api/Models/GetWords',
+    //        //     datatype: 'jsonp',
+    //        beforeSend: function (xhr) {
+    //            //     preloader.css('display', 'block');
+    //        },
+    //        success: function (data) {
+    //            //   console.log(data);
+    //            for (let i = 0; i < data.length; i++) {
+    //                alert(data[i].translate);
+    //            }
+
+    //        },
+    //        error: function (jqXHR, textStatus, errorThrown) {
+    //            alert(jqXHR.responseText || textStatus);
+    //        }
+    //    });
+    //});
+
+    $('#ok').click(function () {
+<<<<<<< HEAD
+        $('#wrapped').fadeOut('1000');
+        setTimeout(function () {
+            $('#main-section').fadeIn('1000');
+        }, 500);
+=======
+        $('#infoBlock').fadeOut('1000');
+        //setTimeout(function () {
+        //    $('#main-section').fadeIn('1000');
+        //}, 500);
+>>>>>>> e9879562c1a8ec453f44adaef41cad8dd2928b96
+    });
+
+    //   $('#getNewWordTry').click(    function (e) {
+    //        e.preventDefault();
+
+    //  });
+    //   $('#Themes').click(function (e) {
+
+    //    e.preventDefault();
+
+    //   });
+    $('#btn-Save').click(function (e) {
+        e.preventDefault();
+<<<<<<< HEAD
+<<<<<<< HEAD
+        var data = {
+            Id_theme: $('#selectTheme')
+        };
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:54049/api/Account/ForgotPassword/',
+            url: 'http://localhost:54049/api/Models/SaveTheme/',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data),
-            beforeSend: function () {
-                preloader.css('display', 'block');
-            },
-            complete: function () {
-                preloader.css('display', 'none');
-            },
-            success: function () {
-                alert("Вам отправлено письмо с новым паролем");
-            },
-            fail: function (data) {
-                alert(data.responseText);
-            },
-            error: function (data) {
-                alert(data.responseText);
-            }
-        });
-    });
-    $('#getItemsButton').click(function (e) {
-        e.preventDefault();
-        
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:54049/api/Models/GetWords',
-            //     datatype: 'jsonp',
             beforeSend: function (xhr) {
-           //     preloader.css('display', 'block');
+                var token = sessionStorage.getItem(tokenKey);
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
             },
             success: function (data) {
-                //   console.log(data);
-                for (var i = 0; i < data.length; i++) {
-                  alert(data[i].translate);
-                }
-                 
+                console.log(data);
+
+
             },
             error: function (jqXHR, textStatus, errorThrown) {
+=======
+=======
+>>>>>>> e9879562c1a8ec453f44adaef41cad8dd2928b96
+        ex.saveSettings($('#selectTheme').val(), sessionStorage.getItem(tokenKey),
+            function () {
+                document.location.href = "main.html";
+            }, function (jqXHR, textStatus, errorThrown) {
+<<<<<<< HEAD
+>>>>>>> ba3c1fe80285efaed5c89ec76a0f8852aa5abe54
+=======
+>>>>>>> e9879562c1a8ec453f44adaef41cad8dd2928b96
                 alert(jqXHR.responseText || textStatus);
-            }
-        });
+            });
     });
-   //   $('#getNewWordTry').click(    function (e) {
-   //        e.preventDefault();
-    
- //  });
-   //   $('#Themes').click(function (e) {
-    
-    //    e.preventDefault();
-       
- //   });
+
     $('#no').click(function (e) {
         e.preventDefault();
         $.ajax({
-            beforeSend: function (xhr) { 
+            beforeSend: function (xhr) {
             },
             success: function (data) {
                 chrome.browserAction.setBadgeText({ text: "" });
@@ -228,41 +238,21 @@ $(document).ready(function () {
 
     $('#yes').click(function (e) {
         e.preventDefault();
-        
+        var data = {
+            Word: document.getElementById("newWord").textContent
+           // Word: $('#newWord').val()
+        };
         $.ajax({
-         //   url: 'http://localhost:54049/api/Models/HaveLerntWord/',
-            type: 'GET',
-            url: 'http://localhost:54049/api/Models/GetUserId/',
-            beforeSend: function (xhr) {             
-                    var token = sessionStorage.getItem(tokenKey);
-                    xhr.setRequestHeader("Authorization", "Bearer " + token); 
-            },
-            success: function (data) {
-               console.log(data);
-               
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert(jqXHR.responseText || textStatus);
-            }
-        });
-    });
-    $('#btn-Save').click(function (e) {
-        e.preventDefault();
-            var data = {
-                Id_theme: $('#selectTheme').val()
-            };
-            $.ajax({
-                type: 'POST',
-                url: 'http://localhost:54049/api/Models/SaveTheme/',
-                contentType: 'application/json; charset=utf-8',
-                data: JSON.stringify(data),
+            url: 'http://localhost:54049/api/Models/SaveWord/',
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
             beforeSend: function (xhr) {
-                var token = sessionStorage.getItem(tokenKey);
-                xhr.setRequestHeader("Authorization", "Bearer " + token);  
+                let token = sessionStorage.getItem(tokenKey);
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
             },
             success: function (data) {
-                   console.log(data);
-                
+                console.log(data);
 
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -270,13 +260,13 @@ $(document).ready(function () {
             }
         });
     });
-    /////////////////////////////////////дальше код, который пока не подключён
+
     $('#logOut').click(function (e) {
         e.preventDefault();
         sessionStorage.removeItem(tokenKey);
+        deleteCookie("username");
+        deleteCookie("password");
+        document.location.href = "autorization.html";
     });
+
 });
-//document.addEventListener('DOMContentLoaded', function () {
-//    document.querySelector('#submitLogin').addEventListener('click', clickHandler);
-//    main();
-//});
